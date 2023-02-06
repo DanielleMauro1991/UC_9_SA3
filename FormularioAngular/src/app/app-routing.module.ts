fc90662c246af9b45f7d63d45e20399968dea101
DanielleMauro16 <eng.danielle.mauro16@gmail.com>
2023-02-06 19:41:48 -0300

    Inclusão login e senha

--- FormularioAngular/src/app/app-routing.module.ts
+++ FormularioAngular/src/app/app-routing.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ContatoComponent } from './views/contato/contato.component';//Importando o componente//
 import { LoginComponent } from './views/login/login.component';
 
+
 const routes: Routes = [
-  {path:"contato", component:ContatoComponent},
   {path:"login", component:LoginComponent}
 ];
 
