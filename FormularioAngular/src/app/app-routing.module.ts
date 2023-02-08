0d96f4aa09a63deba04a485afa15da677beecf6b
DanielleMauro16 <eng.danielle.mauro16@gmail.com>
2023-02-08 16:13:51 -0300

    Carregando Materials

--- FormularioAngular/src/app/app-routing.module.ts
+++ FormularioAngular/src/app/app-routing.module.ts
@@ -3,12 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './views/login/login.component';
 import { ContatoComponent } from './views/contato/contato.component';
 
-
-
 const routes: Routes = [
-  {path:"login", component:LoginComponent},
-  {path:"contato", component:ContatoComponent}
-  
+  {path:"login", component: LoginComponent},
+  {path:"contato", component: ContatoComponent}
 ];
 
 @NgModule({
--- FormularioAngular/src/app/app.module.ts
+++ FormularioAngular/src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './views/login/login.component';
 import { ContatoComponent } from './views/contato/contato.component';
 import { HeaderComponent } from './views/header/header.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './views/login/login.component';
+
 
 
 @NgModule({
@@ -16,15 +17,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppComponent,
     LoginComponent,
     ContatoComponent,
-    HeaderComponent,
-   
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    MatToolbarMododule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
